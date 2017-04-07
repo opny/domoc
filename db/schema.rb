c4a67b8b5a8bae4f3882906a27f1d16d0ec22430
mosaid <moasaid9@gmail.com>
2017-04-07 19:07:26 +0000

    Completed the modeling users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329122310) do
+ActiveRecord::Schema.define(version: 20170407184853) do
 
   create_table "agents", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
